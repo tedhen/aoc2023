2953ac86a5fb5c6dbc3baf6ae98cb38c8596e70e
Ted Henriksson <ted@henrikssons.net>
2023-12-03 23:17:05 +0100

    Completed Day3

--- day3/day3.py
+++ day3/day3.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple, Dict, Any
 
 class PartNumber:
     number = 0
@@ -11,58 +11,89 @@ class PartNumber:
 def find_Parts(input: List[str]) -> List[PartNumber]:
     temp_partnumber = ""
     temp_valid = False
+    temp_loc = None
+    
+    gears_found = {}
     parts = []    
     for y, l in enumerate(input):
         for x, v in enumerate(l):
-            if v == '.':
+            if v.isnumeric():
+                temp_partnumber += v
+                is_valid, loc = check_for_symbols(input, x, y)
+                if is_valid:
+                    temp_valid = True
+                if loc:
+                    temp_loc = loc
+            else:
                 if temp_partnumber:
                     parts.append(PartNumber(int(temp_partnumber), temp_valid))
+                if temp_loc:
+                    add_or_append(gears_found, temp_loc, int(temp_partnumber))
+                temp_loc = None
                 temp_valid = False
                 temp_partnumber = ""
-            elif v.isnumeric():
-                temp_partnumber += v
-                if check_for_symbols(input, x, y):
-                    temp_valid = True
-            
-    if temp_partnumber:
-        parts.append(PartNumber(int(temp_partnumber), temp_valid))
-    return parts
+        
+        if temp_partnumber:
+            parts.append(PartNumber(int(temp_partnumber), temp_valid))
+        if temp_loc:
+            add_or_append(gears_found, temp_loc, int(temp_partnumber))
+         
+        temp_partnumber = ""
+        temp_valid = False
+        temp_loc = None
+    return (parts, gears_found)
 
-
-def check_for_symbols(input: List[str], x:int, y:int) -> bool:
-    # Generate coordinates
+def check_for_symbols(input: List[str], x:int, y:int) -> Tuple[bool,Tuple[int,int]]:
     potential_cords = [(y-1,x-1),(y-1,x),(y-1,x+1),(y,x-1),(y,x+1),(y+1,x-1),(y+1,x),(y+1,x+1)]
-    allowed_cords = [(pot_y, pot_x) for pot_y,pot_x in potential_cords if is_valid_cord(pot_x,pot_y, y_size=len(input), x_size=len(input[y]))]
-    not_allowed_symbols = ['1','2','3','4','5','6','7','8','9','.']
+    allowed_cords = [
+        (pot_y, pot_x)
+        for pot_y,pot_x in potential_cords 
+        if is_valid_coord(pot_x,pot_y, y_size=len(input), x_size=len(input[y]))
+    ]
+    not_allowed_symbols = ['0','1','2','3','4','5','6','7','8','9','.']
     next_to_symbol = False
+    gear_location = None
     for c_y, c_x in allowed_cords:
         try:
-            cord = input[c_y][c_x]
-            if cord not in not_allowed_symbols:
+            coord_value = input[c_y][c_x]
+            if coord_value not in not_allowed_symbols:
                 next_to_symbol = True
+                if coord_value == '*':
+                    gear_location = (c_y, c_x)
         except IndexError:
             continue
 
-    return next_to_symbol
+    return (next_to_symbol, gear_location)
 
-
-def is_valid_cord(x:int, y:int, x_size:int, y_size:int)->bool:
-    if x < 0 or y < 0 or x >= x_size or y >= y_size:
-        return False
-    return True
-
+def is_valid_coord(x:int, y:int, x_size:int, y_size:int)->bool:
+    return not ( x < 0 or y < 0 or x >= x_size or y >= y_size)
 
 def parts_sum(parts: List[PartNumber]) -> int:
-    sum = 0
+    p_sum = 0
     for part in parts:
         if part.is_valid:
-            sum += part.number
-    return sum
+            p_sum += part.number
+    return p_sum
 
+def add_or_append(d:Dict, key: Any,  value: Any)-> None:
+    if key in d.keys():
+        d[key].append(value)
+    else:
+        d[key] = [value]
+
+def calculate_gears(gears: Dict)->int:
+    total_ratio = 0
+    for x in gears.values():
+        if len(x) == 2:
+            new_ratio = x[0] * x[1]
+            total_ratio += new_ratio
+    return total_ratio 
+                
 if __name__ == "__main__":
     input = []
-    with open('day3/test_input.txt') as f:
+    with open('day3/input.txt', encoding="utf8") as f:
         for l in f:
             input.append(l.rstrip())
-    parts = find_Parts(input)
+    parts, gears = find_Parts(input)
     print(f"Part 1: {parts_sum(parts)}")
+    print(f"Part 2: {calculate_gears(gears)}")
