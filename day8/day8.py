b0c79213ac22573f0277e3575e3bce7bdb628f90
Ted Henriksson <ted@henrikssons.net>
2023-12-09 00:56:55 +0100

    Day8

--- day8/day8.py
+++ day8/day8.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+from math import lcm
 
 def validate_puzzle_path(graph: Dict[str,List[str]], start: str, end: str, p_path: List[str])->int:
     travel_length = -1
@@ -6,6 +7,17 @@ def validate_puzzle_path(graph: Dict[str,List[str]], start: str, end: str, p_pat
     while True: 
         travel_length += 1
         if current_node == end:
+            break
+        path_selection = travel_length % len(p_path)
+        current_node = graph[current_node][p_path[path_selection]]
+    return travel_length
+
+def find_ghost_path(graph: Dict[str,List[str]], start: str, p_path: List[str])->int:
+    travel_length = -1
+    current_node = start
+    while True: 
+        travel_length += 1
+        if current_node.endswith('Z'):
             break
         path_selection = travel_length % len(p_path)
         current_node = graph[current_node][p_path[path_selection]]
@@ -21,7 +33,15 @@ def main():
             ext_l = l.strip().translate({ord(c): None for c in '()=,'}).split()
             puzzle_graph[ext_l[0]] = (ext_l[1], ext_l[2])
 
-    print(validate_puzzle_path(puzzle_graph, 'AAA', 'ZZZ', puzzle_path))
+    print(f"Part 1: {validate_puzzle_path(puzzle_graph, 'AAA', 'ZZZ', puzzle_path)}")
+
+    start_nodes = list(filter(lambda x: x.endswith('A'), puzzle_graph.keys()))
+
+    ghost_lengths = []
+    for s in start_nodes:
+        ghost_lengths.append(find_ghost_path(puzzle_graph, s, puzzle_path))
+
+    print(f"Part 2: {lcm(*ghost_lengths)}")
 
 if __name__ == '__main__':
     main()
