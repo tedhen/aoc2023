e4e37ab9a47b782f4b543d7f3bb069bb748c35e7
Ted Henriksson <ted@henrikssons.net>
2023-12-09 12:40:53 +0100

    Light cleanup of Day9

--- day9/day9.py
+++ day9/day9.py
@@ -23,17 +23,14 @@ def predict_previous_value(seq: List[int]) -> int:
     return next_value
 
 def main():
-    puzzle_history = []
     with open('day9/input.txt', encoding='utf8') as f:
         temp = [x.strip().split() for x in f]
         puzzle_history = list(map(lambda t: [int(x) for x in t], temp))
 
     predict_next_values = [predict_next_value(x) for x in puzzle_history]
-
     print(f"Part 1: {sum(predict_next_values)}")
 
     predict_previous_values = [predict_previous_value(x) for x in puzzle_history]
-
     print(f"Part 1: {sum(predict_previous_values)}")
 
 if __name__ == '__main__':
