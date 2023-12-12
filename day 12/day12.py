3b7c9aeddfbc9fb8af008846063ff3d826cdb5c5
Ted Henriksson <ted@henrikssons.net>
2023-12-13 00:03:08 +0100

    A start to Day12, need to fix the recursion rules.

---
+++ day 12/day12.py
@@ -0,0 +1,52 @@
+import re
+
+def find_arrangement(report, groups):
+
+    arrangements = 0
+
+    if len(report) == 0 and len(groups) > 0:
+        return 0
+    elif len(report) == 0 and len(groups) == 0:
+        return 1
+    elif len(report) > 0 and len(groups) == 0:
+        return 0
+
+    cur = report[0]
+    if cur == '.':
+        arrangements = find_arrangement(report[1:], groups)
+    elif cur == '?':
+        operational = '.' + report[1:]
+        damaged = '#' + report[1:]
+        arrangements = find_arrangement(operational, groups) + find_arrangement(damaged, groups)
+    elif cur == '#':
+        g = groups[0]
+        damaged = [ True for x in report[:g] if x == '#']
+
+        try:
+            if len(damaged) < 
+            if damaged and report[g] != '#':
+                arrangements = find_arrangement(report[g:], groups[1:])
+        except IndexError:
+            pass
+
+    else:
+        raise ValueError('Unknown type of char')
+
+    return arrangements
+
+def main():
+    with open('day 12/test_input.txt', encoding='utf8') as f:
+        puzzel_input = []
+        for l in f:
+            report = re.match(r"[.?#]*", l).group(0)
+            groups = re.findall(r'\d', l)
+            puzzel_input.append((report, list(map(int,groups))))
+        
+
+
+    x = find_arrangement(*puzzel_input[0])
+    print(x)
+
+
+if __name__ == '__main__': 
+    main()
